716858abe10963b6d71216bd913ebf53f4059f2e
gmsdev04 <gmsdev04@icloud.com>
2019-11-15 11:38:55 -0200

    concluindo primeira versão do tokens

--- SecurityTokenService/src/main/java/com/br/gmsdev04/sts/controllers/TokenController.java
+++ SecurityTokenService/src/main/java/com/br/gmsdev04/sts/controllers/TokenController.java
@@ -1,31 +1,32 @@
 package com.br.gmsdev04.sts.controllers;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.gmsdev04.commons.ApiRoundTrip;
 import com.br.gmsdev04.sts.entities.Application;
-import com.br.gmsdev04.sts.entities.Session;
+import com.br.gmsdev04.sts.entities.Token;
 import com.br.gmsdev04.sts.repository.ApplicationRepository;
-import com.br.gmsdev04.sts.repository.SessionRepository;
+import com.br.gmsdev04.sts.repository.TokenRepository;
 
-@RestController("/sts/v1/tokens")
+@RestController()
 public class TokenController {
 
 	@Autowired
-	private SessionRepository sessions;
+	private TokenRepository tokens;
 
 	@Autowired
 	private ApplicationRepository applications;
 	
-	@GetMapping()
-	public ResponseEntity<ApiRoundTrip> onGet(
+	@PostMapping("/sts/v1/tokens")
+	public ResponseEntity<ApiRoundTrip> onPost(
 			@RequestHeader("client_id") String clientId,
 			@RequestHeader("client_secret") String clientSecret,
 			@RequestHeader("grant_type") String grantType
@@ -36,22 +37,30 @@ public class TokenController {
 		    (grantType == null    || grantType.isEmpty()) 	
 		  )
 			return ResponseEntity.badRequest().body(new ApiRoundTrip("cliend_id, client_secret and grant_type can not be null or empty"));
-
 		
-		Application ap = new Application();
+		if(grantType.equals("password")) 
+		{
+			Optional<Application> apl = applications.findById(clientId);
+			
+			if(apl.isPresent()) {
+				Application application = apl.get();
+				
+				if(application.getClientSecret().toHexString().equals(clientSecret)) {
+					Token token = new Token();
+					
+					tokens.save(token);
+					
+					return ResponseEntity.created(null).body(new ApiRoundTrip("Token gerado com sucesso", token));
+				}
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+			}
+			
+		}else if (grantType.equals("refresh_token")) 
+		{
+			
+		}
 		
-		applications.save(ap);
-		
-		Session s = new Session();
-		String id = UUID.randomUUID().toString();
-		s.setJwtHash(id);
-		
-		sessions.save(s);
-		
-		Optional<Session> ss = sessions.findById(id);
-
-		
-		return null;
+		return ResponseEntity.badRequest().body(new ApiRoundTrip("grant_type inválid"));
 	}
 
 }
--- SecurityTokenService/src/main/java/com/br/gmsdev04/sts/repository/TokenRepository.java
+++ SecurityTokenService/src/main/java/com/br/gmsdev04/sts/repository/TokenRepository.java
@@ -3,10 +3,10 @@ package com.br.gmsdev04.sts.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.br.gmsdev04.sts.entities.Session;
+import com.br.gmsdev04.sts.entities.Token;
 
 @Repository
-public interface SessionRepository extends CrudRepository<Session,String> {
+public interface TokenRepository extends CrudRepository<Token,String> {
 
 	String findByJwtHash(String jwtHash);	
 	
